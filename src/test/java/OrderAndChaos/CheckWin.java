f4a1eb779df52b8d2edb6d5851b8d9c8d34e13c9
pierbin24 <pierbin24@gmail.com>
2024-01-10 18:39:10 +0100

    WIP Testing

--- src/test/java/OrderAndChaos/CheckWin.java
+++ src/test/java/OrderAndChaos/CheckWin.java
@@ -43,7 +43,7 @@ public class CheckWin {
         {' ', 'O', ' ', ' ', ' ', ' '},
         {' ', 'O', ' ', ' ', ' ', ' '},
         {' ', 'O', ' ', ' ', ' ', ' '},
-        {' ', 'O', ' ', ' ', ' ', ' '}
+        {' ', ' ', ' ', ' ', ' ', ' '}
     };
     testBoards.add(testBoard);
 
@@ -192,5 +192,17 @@ public class CheckWin {
     };
     assertFalse(game.checkWin());
   }
+
+
+  @ParameterizedTest
+  @CsvSource({
+      "0,2,1,2,2,2,3,2,4,2,5",
+      "1,1,0,1,1,1,2,1,3,1,4",
+      "2,0,0,1,1,2,2,3,3,4,4",
+  })
+  public void testWinningStreak(){
+
+  }
+
 
 }
