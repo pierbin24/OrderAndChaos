00a6994c64f94baf295c0b33ae03ed01cef60aa3
pierbin24 <pierbin24@gmail.com>
2024-01-10 01:53:16 +0100

    WIP Testing

--- src/test/java/OrderAndChaos/CheckWin.java
+++ src/test/java/OrderAndChaos/CheckWin.java
@@ -4,9 +4,12 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.ArrayList;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -113,17 +116,20 @@ public class CheckWin {
 
   }
 
+
   @BeforeEach
   void setUp() {
     game = new Game();
     board = new char[6][6];
   }
 
+
   @Test
   public void testCheckRow() {
     game.board = testBoards.get(0);
     assertTrue(game.checkRow());
   }
+
 
   @Test
   public void testCheckCol() {
@@ -155,6 +161,25 @@ public class CheckWin {
     assertTrue(game.checkDiag2(i,j));
   }
 
+
+  @ParameterizedTest
+  @MethodSource("provideWinningTestCases")
+  public void testOrderWin(char[][] board) {
+    game.board = board;
+    assertTrue(game.checkWin());
+  }
+
+  private static Stream<Arguments> provideWinningTestCases() {
+    Stream.Builder<Arguments> builder = Stream.builder();
+
+    for (int i = 0; i < 8; i++) {
+      builder.add(Arguments.of((Object) testBoards.get(i)));
+    }
+
+    return builder.build();
+  }
+
+
   @Test
   public void testChaosWin(){
     game.board = new char[][]{
@@ -169,105 +194,3 @@ public class CheckWin {
   }
 
 }
-
-
-
-//@ExtendWith(MockitoExtension.class)
-
-
-  //@Mock
-  //private Scanner mockScanner;
-
-  /*
-
-    Game game;
-  char[][] board;
-
-  @BeforeEach
-  void setUp() {
-    game = new Game();
-    board = new char[6][6];
-    game.initializeBoard(board);
-  }
-
-
-  @ParameterizedTest
-  @CsvSource({
-      "1, 2, X",
-      "3, 4, O",
-      "5, 5, X",
-      // Aggiungi altri casi di test come necessario
-  })
-  void makeTurn_ValidInput(int row, int col, char type) {
-    game.makeTurn(board, String.format("%d,%d,%s", row, col, type));
-
-    assertEquals(type, board[row][col]);
-    // Assicurati che il metodo isValidMove sia chiamato con gli argomenti corretti
-    // Assicurati che il metodo printBoard sia chiamato con il tuo stato atteso del tavolo
-    // Altri controlli che desideri fare
-  }
-
-  @ParameterizedTest
-  @CsvSource({
-      "invalid input",
-      "7, 8",
-      "X, 2, 3",
-      "0, 6, X"
-  })
-  void makeTurn_InvalidInput(String input) {
-
-    game.makeTurn(board, input);
-
-    // Verifica che la matrice board sia rimasta invariata (nessun aggiornamento)
-    char[][] expectedBoard = new char[6][6];
-    game.initializeBoard(expectedBoard);
-    assertArrayEquals(expectedBoard, board);
-  }
-
-   */
-
-
-
-
-
-
-
-
-
-
-
-  /*
-
-  @Test
-  void makeTurn_ValidInput_UpdatesBoard() {
-    char[][] board = new char[6][6];
-    when(mockScanner.nextLine()).thenReturn("1,2,X");
-    game.makeTurn(board, mockScanner, true);
-    assertEquals('X', board[1][2]);
-    verify(mockScanner, times(1)).nextLine();
-  }
-  @Test
-  void makeTurn_ValidInput_UpdatesBoard() {
-    char[][] board = new char[6][6];
-    when(mockScanner.nextLine()).thenReturn("1,2,X");
-    game.makeTurn(board, mockScanner, true);
-    assertEquals('X', board[1][2]);
-    verify(mockScanner, times(1)).nextLine();
-  }
-
-    @Test
-  void makeTurn_InvalidInput_PrintsErrorMessage() {
-    char[][] board = new char[6][6];
-    when(mockScanner.nextLine()).thenReturn("invalid input");
-    game.makeTurn(board, mockScanner, true);
-    verify(mockScanner, times(1)).nextLine();
-  }
-
-  @Test
-  void makeTurn_ExceptionDuringConversion_PrintsErrorMessage() {
-    char[][] board = new char[6][6];
-    when(mockScanner.nextLine()).thenReturn("1,2,invalid");
-    assertThrows(NumberFormatException.class, () -> game.makeTurn(board, mockScanner, true));
-    verify(mockScanner, times(1)).nextLine();
-  }
-   */
